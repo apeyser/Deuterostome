066c1d3b4a7b0517dda3a4ec320b1f1e1bb6f4a0
ap <ap@71cd2e2a-5bfc-0310-b9d0-8a5a8567c16d>
2006-04-18 18:24:53 +0000

    git-svn-id: svn://disks/svn/dm-3.2/trunk@487 71cd2e2a-5bfc-0310-b9d0-8a5a8567c16d

--- plugins/stats.d
+++ plugins/stats.d
@@ -7,7 +7,6 @@
 static L max_n = 0;
 #define max_kn \(1000\)
 static D binom[max_kn+1][max_kn+1] = {1};
-static D binom_[max_kn+1] = {1};
 ) def
 
 
@@ -40,21 +39,15 @@ static D binom_[max_kn+1] = {1};
     return OK;
   }
 
-  if \(n != max_n+1\)
-    for \(i = 0; i <= max_n; ++i\)
-      binom_[i] = binom[max_n][i];
-
   for \(i = max_n+1; i <= n; ++i\) {
-    binom_[i] = 1;
-    for \(j = i-1; j; --j\) {
-      binom_[j] += binom_[j-1];
+    binom[i][0] = binom[i][i] = 1;
+    for \(j = 1; j < i; ++j\) {
+      binom[i][j] = binom[i-1][j-1] + binom[i-1][j];
     }
-    for \(j = 0; j <= n; ++j\)
-      binom[i][j] = binom_[j];
   }
   max_n = n;
 
-  *\(D*\) NUM_VAL\(o_1\) = binom_[n];
+  *\(D*\) NUM_VAL\(o_1\) = binom[n][k];
   return OK;
 )}
     ]
