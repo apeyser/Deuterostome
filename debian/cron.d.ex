12093633f264774121773b74e8c35ffb49e5ba74
ap <ap@71cd2e2a-5bfc-0310-b9d0-8a5a8567c16d>
2008-12-12 16:27:58 +0000

    git-svn-id: svn://disks/svn/dm-4/trunk@1661 71cd2e2a-5bfc-0310-b9d0-8a5a8567c16d

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dm package
-#
-0 4	* * *	root	dm_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dm):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="dm" command="/usr/bin/dm"
