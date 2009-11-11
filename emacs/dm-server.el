433d5aca012c328984aa73afe3117d9a9ca8cbdd
Alex Peyser <peyser.alex@gmail.com>
2009-11-11 18:59:27 -0500

    Redid dm-server binding

--- emacs/dm-server.el
+++ emacs/dm-server.el
@@ -1,26 +1,28 @@
 (require 'server)
 
-(defun dm-server-get-default (sym-name default)
-  (let ((new-sym (intern-soft (format "%s-default-value" sym-name))))
-    (if (and new-sym (boundp new-sym)) new-sym default)))
+(defun dm-server-get-default (sym-name default sym-default)
+  (let ((new-sym (intern-soft (format "%s-default" sym-name))))
+    (if (and new-sym (boundp new-sym)) (symbol-value new-sym)
+      (if default (symbol-value sym-default)))))
 
-(defun dm-server-make-sym (sym use-sym check-bound)
+(defun dm-server-make-sym (sym check-bound default)
   (when (boundp sym)
     (let ((sym-name (symbol-name sym)))
       (let* ((dm-sym-name (format "dm-%s" sym-name))
 	     (dm-sym (intern dm-sym-name)))
 	(when (not (and check-bound (boundp dm-sym)))
 	  (make-local-variable sym)
-	  (set sym (dm-server-get-default dm-sym-name (if use-sym sym nil)))
+	  (set sym (dm-server-get-default dm-sym-name default sym))
 	  (defvaralias dm-sym sym))))))
 
 (defun dm-server-get-syms ()
-  (mapatoms (lambda (sym)
-	     (when (string-match "^server\\(-.*\\)?$" (symbol-name sym))
-	       (dm-server-make-sym sym nil t)))))
+  (mapatoms 
+   (lambda (sym)
+     (when (string-match "^server\\(-.*\\)?$" (symbol-name sym))
+       (dm-server-make-sym sym t nil)))))
 
-(defun dm-server-set-syms (check-bound syms)
-  (mapc (lambda (sym) (dm-server-make-sym sym t check-bound)) syms))
+(defun dm-server-set-syms (&rest syms)
+  (mapc (lambda (sym) (dm-server-make-sym sym nil t)) syms))
 
 (defvar   dm-server-start                    nil)
 (defconst dm-server-buffer-default           " *dm-server*")
@@ -32,14 +34,15 @@
 ;; server-raise-frame server-kill-new-buffers
 (defun dm-server-start (nm)
   (when (not dm-server-start)
-    (dm-server-set-syms t
-     '(server-auth-dir 
-       server-socket-dir
-       server-log-time-function
-       server-temp-file-regexp
-       server-raise-frame
-       server-kill-new-buffers))
-    (dm-server-set-syms nil '(server-name server-process))
+    (dm-server-set-syms
+     'server-auth-dir 
+     'server-socket-dir
+     'server-log-time-function
+     'server-temp-file-regexp
+     'server-raise-frame
+     'server-kill-new-buffers
+     'server-name 
+     'server-process)
     (dm-server-get-syms)
     (setq dm-server-start t))
    
