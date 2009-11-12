36b6256055b61335a5ea7bd15551a213cc239276
Alex Peyser <peyser.alex@gmail.com>
2009-11-12 02:27:34 -0500

    cleaned up dm-server-source

--- emacs/dm-server-source.el
+++ emacs/dm-server-source.el
@@ -5,20 +5,21 @@
     (dm-server-force-delete)
     (message nil)
     (dm-server-start)
-    ;;(process-put dm-server-process :dm-server-buffer (current-buffer))
     (add-hook 'kill-buffer-hook (lambda () (dm-server-start t)) t t)))
 ")
 (defun build ()
   (catch 'ret
     (mapc 
      (lambda (dir)
-       (let ((path (convert-standard-filename
-		    (concat (if dir dir ".") "/server.el"))))
+       (let ((path (concat 
+		    (file-name-as-directory 
+		     (expand-file-name (if dir dir ".")))
+		    "server.el")))
 	 (when (file-readable-p path)
 	   (set-buffer (get-buffer-create "*server*"))
 	   (insert-file-contents path nil nil nil t)
 	   (goto-char (point-min))
-	   (while (re-search-forward "server" nil t)
+	   (while (search-forward "server" nil t)
 	     (when (not (looking-back ":server"))
 	       (replace-match "dm-server" nil t)))
 	   (goto-char (point-max))
